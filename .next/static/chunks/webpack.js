1a0a94b4f5a27e013a0b5a1f7fe6810de339d00e
JasonLy <85002298+Jason-Fallon@users.noreply.github.com>
2021-09-19 11:13:35 +0700

    Updata layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a43908204450ccf7cabc";
+/******/ 	var hotCurrentHash = "2f4a10bfa33c015c1a43";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
