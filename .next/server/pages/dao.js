50e6ed94f26e55712595661aa2e36fde76e294c7
JasonLy <85002298+Jason-Fallon@users.noreply.github.com>
2021-10-01 14:11:09 +0700

    Adjust layout

--- .next/server/pages/dao.js
+++ .next/server/pages/dao.js
@@ -49,7 +49,7 @@ module.exports =
 /******/
 /******/ 		// "0" is the signal for "already loaded"
 /******/ 		if(installedChunks[chunkId] !== 0) {
-/******/ 			var chunk = require("../" + ({}[chunkId]||chunkId) + "." + {"44":"1adb8b81dce2995cb6c3"}[chunkId] + ".js");
+/******/ 			var chunk = require("../" + ({}[chunkId]||chunkId) + "." + {"44":"1f8885ee3c6dcd7328d2"}[chunkId] + ".js");
 /******/ 			var moreModules = chunk.modules, chunkIds = chunk.ids;
 /******/ 			for(var moduleId in moreModules) {
 /******/ 				modules[moduleId] = moreModules[moduleId];
