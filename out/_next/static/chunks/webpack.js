a74267cb3c5a9c9d636ce1446d49405b77ed0c5b
JasonLy <85002298+Jason-Fallon@users.noreply.github.com>
2021-10-04 17:54:43 +0700

    Create run build

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2ef1d38d076e78723e2a";
+/******/ 	var hotCurrentHash = "593b9df3e611d2e83bc8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
