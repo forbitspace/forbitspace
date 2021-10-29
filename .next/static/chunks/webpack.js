327530f0af50664032b14331e73b9042c4b2a28b
JasonLy <85002298+Jason-Fallon@users.noreply.github.com>
2021-10-29 16:44:31 +0700

    Update code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f4fcdf2d8329594f62f6";
+/******/ 	var hotCurrentHash = "0a3634381e7b6a4aaf82";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
