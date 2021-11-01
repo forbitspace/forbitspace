878b33be022a2d8f3770d8ff02f5a51081a3d73c
JasonLy <85002298+Jason-Fallon@users.noreply.github.com>
2021-11-01 12:24:03 +0700

    Update footer, liquidity section

--- client/components/Footer/constants.js
+++ client/components/Footer/constants.js
@@ -1,159 +1,169 @@
 export const DataSocialLinkFooter = [
-  {
-    link: "https://twitter.com/forbitspace",
-    url: "../images/icon-social-footer/6.svg",
-  },
-  {
-    link: "https://t.me/forbitspace",
-    url: "../images/icon-social-footer/5.svg",
-  },
-  {
-    link: "https://github.com/forbitspace",
-    url: "../images/icon-social-footer/4.svg",
-  },
-  {
-    link: "https://medium.com/@forbitspace",
-    url: "../images/icon-social-footer/1.svg",
-  },
-  {
-    link: "https://discord.com/invite/forbitspace",
-    url: "../images/icon-social-footer/3.svg",
-  },
-  {
-    link: "https://www.reddit.com/r/forbitspace/",
-    url: "../images/icon-social-footer/2.svg",
-  },
-  {
-    link: "https://www.youtube.com/channel/UCU9ejvfmL6p2Pdwp2-q163w",
-    url: "../images/icon-social-footer/7.svg",
-  },
+    {
+        link: "https://twitter.com/forbitspace",
+        url: "../images/icon-social-footer/6.svg",
+    },
+    {
+        link: "https://t.me/forbitspace",
+        url: "../images/icon-social-footer/5.svg",
+    },
+    {
+        link: "https://github.com/forbitspace",
+        url: "../images/icon-social-footer/4.svg",
+    },
+    {
+        link: "https://medium.com/@forbitspace",
+        url: "../images/icon-social-footer/1.svg",
+    },
+    {
+        link: "https://discord.com/invite/forbitspace",
+        url: "../images/icon-social-footer/3.svg",
+    },
+    {
+        link: "https://www.reddit.com/r/forbitspace/",
+        url: "../images/icon-social-footer/2.svg",
+    },
+    {
+        link: "https://www.youtube.com/channel/UCU9ejvfmL6p2Pdwp2-q163w",
+        url: "../images/icon-social-footer/7.svg",
+    },
 ];
 export const navContent = [
-  {
-    link: "",
-    nav: "Space Trade",
-    contents: [
-      {
-        link: "/aggregation",
-        content: "Aggregation Protocol",
-      },
-      {
-        link: "/liquidity",
-        content: "Liquidity Protocol",
-      },
-      {
-        link: "/limit",
-        content: "Limit Order Protocol",
-      },
-      {
-        link: "/derivative",
-        content: "Derivative Protocol",
-      },
-    ],
-  },
-  {
-    link: "",
-    nav: "Governance",
-    contents: [
-      // {
-      //   link: "/token", //.../images/pdf/fbs-token-1.pdf
-      //   content: "FBS Token",
-      //   outLink: true,
-      //   disable: true,
-      // },
-      {
-        link: "/dao",
-        content: "Space Dao",
-      },
-      // {
-      //   link: "",
-      //   content: "Pools",
-      //   disable: true,
-      // },
-      // {
-      //   link: "",
-      //   content: "Farming",
-      //   disable: true,
-      // },
-      // {
-      //   link: "",
-      //   content: "Stacking",
-      //   disable: true,
-      // },
-    ],
-  },
-  {
-    link: "",
-    nav: "Developers",
-    contents: [
-      {
+    {
         link: "",
-        content: "Documentation",
-      },
-      {
-        link: "../forbitspace-whitepaper.pdf",
-        content: "Whitepaper",
-        outLink: true,
-      },
-      {
-        link: "https://github.com/forbitspace",
-        content: "Github",
-        outLink: true,
-      },
-      // {
-      //   link: "",
-      //   content: "Audits",
-      //   disable: true,
-      // },
-      {
+        nav: "Space Trade",
+        contents: [
+            {
+                link: "/aggregation",
+                content: "Aggregation Protocol",
+            },
+            {
+                link: "/liquidity",
+                content: "Liquidity Protocol",
+            },
+            {
+                link: "/limit",
+                content: "Limit Order Protocol",
+            },
+            {
+                link: "/derivative",
+                content: "Derivative Protocol",
+            },
+        ],
+    },
+    {
         link: "",
-        content: "Bug bounty",
-        disable: true,
-      },
-    ],
-  },
-  {
-    nav: "Self-custody",
-    link: "/selfcustody",
-    contents: [
-      {
-        link: "/bridge",
-        content: "Bridge Space",
-      },
-      {
-        link: "/mobile-wallet",
-        content: "Mobile wallet",
-      },
-      {
-        link: "/smart-contract",
-        content: "Smart contract wallet",
-      },
-      {
-        link: "/assets",
-        content: "Asset management",
-      },
-    ],
-  },
-  {
-    nav: "Support",
-    // disable: true,
-    link: "",
-    contents: [
-      {
-        link: "https://help.forbitspace.com/",
-        content: "Help Center",
-        outLink: true,
-      },
-      {
-        link: "../documents/terms-of-service.pdf",
-        content: "Term of Service",
-        outLink: true,
-      },
-      {
-        link: "../documents/privacy-policy.pdf",
-        content: "Privacy Policy",
-        outLink: true,
-      },
-    ],
-  },
+        nav: "Governance",
+        contents: [
+            // {
+            //   link: "/token", //.../images/pdf/fbs-token-1.pdf
+            //   content: "FBS Token",
+            //   outLink: true,
+            //   disable: true,
+            // },
+            {
+                link: "/dao",
+                content: "Space Dao",
+            },
+            // {
+            //   link: "",
+            //   content: "Pools",
+            //   disable: true,
+            // },
+            // {
+            //   link: "",
+            //   content: "Farming",
+            //   disable: true,
+            // },
+            // {
+            //   link: "",
+            //   content: "Stacking",
+            //   disable: true,
+            // },
+        ],
+    },
+    {
+        link: "",
+        nav: "Developers",
+        contents: [
+            {
+                link: "",
+                content: "Documentation",
+            },
+            {
+                link: "../forbitspace-whitepaper.pdf",
+                content: "Whitepaper",
+                outLink: true,
+            },
+            {
+                link: "https://github.com/forbitspace",
+                content: "Github",
+                outLink: true,
+            },
+            {
+                link: "",
+                content: "Audit",
+                disable: true,
+            },
+            {
+                link: "",
+                content: "Bug bounty",
+                disable: true,
+            },
+        ],
+    },
+    {
+        nav: "Self-custody",
+        link: "/selfcustody",
+        contents: [
+            {
+                link: "/bridge",
+                content: "Bridge Space",
+            },
+            {
+                link: "/mobile-wallet",
+                content: "Mobile wallet",
+            },
+            {
+                link: "/smart-contract",
+                content: "Smart contract wallet",
+            },
+            {
+                link: "/assets",
+                content: "Asset management",
+            },
+        ],
+    },
+    {
+        nav: "Support",
+        // disable: true,
+        link: "",
+        contents: [
+            {
+                link: "https://blog.forbitspace.com/",
+                content: "Blog",
+                outLink: true,
+            },
+            {
+                link: "https://help.forbitspace.com/",
+                content: "Help Center",
+                outLink: true,
+            },
+            {
+                link: "../documents/terms-of-service.pdf",
+                content: "Term of Service",
+                outLink: true,
+            },
+            {
+                link: "../documents/privacy-policy.pdf",
+                content: "Privacy Policy",
+                outLink: true,
+            },
+            {
+                link: "",
+                content: "Press & Brand",
+                // outLink: true,
+            },
+        ],
+    },
 ];
