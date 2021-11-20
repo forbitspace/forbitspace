c04a8b72a27b62ad60a45158af2fce03828802eb
Jason Ly <85002298+Jason-Fallon@users.noreply.github.com>
2021-11-20 19:51:03 +0700

    Adjust description

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -861,9 +861,6 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       name: "theme-color",
       content: "#fff"
     }), __jsx("meta", {
-      name: "Description",
-      content: "The forbitspace DEX super aggregator is an interoperability protocol that connects disparate blockchains. That unites decentralized applications across disparate blockchains, simultaneously uniting liquidity across chains and making it possible to transact in, out, and across disparate networks with ease."
-    }), __jsx("meta", {
       property: "og:image",
       content: "https://ipfs.infura.io/ipfs/QmaioLHcm1ugkc36oSQ1zyg56W2sqiPhWxvB1xtK2egEWs"
     }), __jsx("meta", {
