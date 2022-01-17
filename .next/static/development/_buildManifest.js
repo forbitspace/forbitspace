63a25afd6c661c59f2cf2f0006987377169b867b
Jason Ly <85002298+Jason-Fallon@users.noreply.github.com>
2022-01-17 16:10:59 +0700

    Update interface

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/mobile-wallet":["static\u002Fchunks\u002Fpages\u002Fmobile-wallet.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmobile-wallet"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
