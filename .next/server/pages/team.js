a44112efb3fb700c600581361abd0a20680354af
pantinho <pantinho.space@gmail.com>
2022-10-26 14:57:58 +0700

    update information of the contributer

--- .next/server/pages/team.js
+++ .next/server/pages/team.js
@@ -49,7 +49,7 @@ module.exports =
 /******/
 /******/ 		// "0" is the signal for "already loaded"
 /******/ 		if(installedChunks[chunkId] !== 0) {
-/******/ 			var chunk = require("../" + ({}[chunkId]||chunkId) + "." + {"42":"61986bb9cf4b7aa0d4a4"}[chunkId] + ".js");
+/******/ 			var chunk = require("../" + ({}[chunkId]||chunkId) + "." + {"42":"3ad3d399416e4d0d29e4"}[chunkId] + ".js");
 /******/ 			var moreModules = chunk.modules, chunkIds = chunk.ids;
 /******/ 			for(var moduleId in moreModules) {
 /******/ 				modules[moduleId] = moreModules[moduleId];
